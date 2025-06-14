43c9cf4929705ca1d88efa92d53b7cc7e6480727
Santiago Serrano <sant.serrano@icloud.com>
2025-06-13 23:41:43 -0300

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,29 +1,21 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-// Obtener config base
 const config = getDefaultConfig(__dirname);
 
-// Asegurar estructuras existentes
+// Asegurarse de que las propiedades existen
 config.resolver = config.resolver || {};
-config.resolver.sourceExts = config.resolver.sourceExts || [];
-config.resolver.alias = config.resolver.alias || {};
-config.transformer = config.transformer || {};
+config.resolver.alias = {
+  'react-native-svg': 'react-native-svg/lib/commonjs/ReactNativeSVG.web.js',
+  '@': __dirname
+};
 
-// Agregar alias
-config.resolver.alias['react-native-svg'] = 'react-native-svg/lib/commonjs/ReactNativeSVG.web.js';
-config.resolver.alias['@'] = __dirname;
-
-// Agregar plataformas
 config.resolver.platforms = ['web', 'native', 'ios', 'android'];
+config.resolver.sourceExts = [...(config.resolver.sourceExts || []), 'ts', 'tsx'];
 
-// Extensiones
-config.resolver.sourceExts.push('ts', 'tsx');
-
-// Configuración del transformer
+config.transformer = config.transformer || {};
 config.transformer.assetPlugins = ['expo-asset/tools/hashAssetFiles'];
 config.transformer.babelTransformerPath = require.resolve('metro-react-native-babel-transformer');
 
-// Resolver campos principales
 config.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
 
 module.exports = config;
